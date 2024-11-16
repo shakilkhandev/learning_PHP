a6936ecd8a531e175d795a2b5df5bf1a75a8bc2e
Shakil Khan <60468711+shakilkhandev@users.noreply.github.com>
2024-11-17 05:56:08 +0600

    Update isLeapYear.php
    Years are not leap years(Ex:1900) only if divisible by 400.(Ex:2000) 
    this is why change has been made to the condition
    
    if ($year%4 !== 0 || ($year%100 == 0 && $year%400 !=0 ))

--- isLeapYear.php
+++ isLeapYear.php
@@ -1,9 +1,9 @@
 <?php
-    # leap year determining: Note: Century Years are not leap years.
+    # leap year determining: Note: Century Years are not leap years(Ex:1900) only if divisible by 400.(Ex:2000) 
 
     function isLeapYear($year){
 
-        if ($year%4 !== 0 || $year%100 == 0  ){
+        if ($year%4 !== 0 || ($year%100 == 0 && $year%400 !=0 )){
             echo $year . " is not a leap year.";
         }else{
                echo $year . " is a leap year.";
